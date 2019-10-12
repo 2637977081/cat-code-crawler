5a31b44c702e7086614c68605015bad8a72250ca
lvgang <lvgang@golaxy.cn>
2019-10-12 18:57:56 +0800

    提交初始代码

---
+++ cat-code-job-admin/src/main/java/com/cat/code/job/admin/CrawlerJobAdminApplication.java
@@ -0,0 +1,17 @@
+package com.cat.code.job.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: lvgang
+ * @Time: 2019/9/24 11:13
+ * @Email: lvgang@golaxy.cn
+ * @Description: todo
+ */
+@SpringBootApplication
+public class CrawlerJobAdminApplication {
+    public static void main(String[] args){
+        SpringApplication.run(CrawlerJobAdminApplication.class,args);
+    }
+}
